4f2fb86dadfad70469230f39aa140ec370ec11c8
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-09-30 21:41:43 +0600

    Initial Commit

---
+++ ColorMatch2/ColorMatch2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatch2App.swift
+//  ColorMatch2
+//
+//  Created by Olzhas Zhakan on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMatch2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorMatch2
+//
+//  Created by Olzhas Zhakan on 30.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
