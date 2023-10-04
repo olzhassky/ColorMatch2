0b400a2fa0456d12fcaddc02134299707281a641
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-10-04 14:50:18 +0600

    test1

--- ColorMatch2/ContentView.swift
+++ ColorMatch2/ContentView.swift
@@ -8,17 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newColors: [Color] = []
+    let columns: [GridItem] = [
+        GridItem(.fixed(70), spacing: nil, alignment: nil),
+        GridItem(.fixed(70), spacing: nil, alignment: nil),
+        GridItem(.fixed(70), spacing: nil, alignment: nil),
+        GridItem(.fixed(70), spacing: nil, alignment: nil)
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            LazyVGrid(columns: columns) {
+                ForEach(0..<newColors.count, id: \.self) { index in
+                    Rectangle()
+                        .frame(height: 70)
+                        .foregroundColor(newColors[index])
+                
+                }
+            }
+            .navigationBarTitle("Random Color", displayMode: .large)
         }
-        .padding()
+        Button("Random") {
+            generateColors()
+        }
+        .buttonStyle(.bordered)
+        .cornerRadius(13)
+        .padding(50)
+
     }
+    func generateColors() {
+        var colors: [Color] = []
+        for i in 0..<14 {
+            let randomColor = Color (
+                            red: Double.random(in: 0...1),
+                            green: Double.random(in: 0...1),
+                            blue: Double.random(in: 0...1)
+            )
+            colors.append(randomColor)
+        }
+        let duplicatedColor = Color(
+            red: Double.random(in: 0...1),
+            green: Double.random(in: 0...1),
+            blue: Double.random(in: 0...1)
+        )
+        colors.append(duplicatedColor)
+        colors.append(duplicatedColor)
+        print(duplicatedColor)
+        colors.shuffle()
+        newColors = colors
+        }
+    
 }
+
 
 #Preview {
     ContentView()
 }
+
+
